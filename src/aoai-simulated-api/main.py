c8cd19e1f36693c0ab1e6471bae26fcef23752fb
Stuart Leeks <stuartle@microsoft.com>
2024-02-01 12:03:59 +0000

    Convert to requests

--- src/aoai-simulated-api/main.py
+++ src/aoai-simulated-api/main.py
@@ -1,5 +1,5 @@
-import httpx
 import os
+import requests
 from fastapi import FastAPI, Request
 
 api_key = os.getenv("AZURE_OPENAI_KEY")
@@ -29,25 +29,20 @@ async def catchall(request: Request):
     fwd_headers["api-key"] = api_key
 
     # create new HTTP request to api_endpoint. copy headers, body, etc from request
-    async with httpx.AsyncClient() as client:
-        url = api_endpoint
-        if url.endswith("/"):
-            url = url[:-1]
-        url += request.url.path + "?" + request.url.query
-        print("Forwarding to: " + url, flush=True)
-        print("Forwarding headers: " + str(fwd_headers), flush=True)
-        
-        response = await client.request(
-            request.method,
-            url,
-            headers=fwd_headers,
-            data=body
-        )
-        print(f"Response ({response.status_code}):", flush=True)
-        print(response.headers, flush=True)
-        print(response.text, flush=True)
-        
-        # TODO set headers, status code, etc?
-        return response.text
+    url = api_endpoint
+    if url.endswith("/"):
+        url = url[:-1]
+    url += request.url.path + "?" + request.url.query
+    print("Forwarding to: " + url, flush=True)
+    print("Forwarding headers: " + str(fwd_headers), flush=True)
 
-
+    response = requests.request(
+        request.method,
+        url,
+        headers=fwd_headers,
+        data=body
+    )
+    print(f"Response ({response.status_code}):", flush=True)
+    print(response.headers, flush=True)
+    print(response.text, flush=True)
+    return response.text
