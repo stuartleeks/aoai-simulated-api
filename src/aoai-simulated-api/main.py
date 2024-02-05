412d3ff6cfd8d76a367bd9f9851bde0eca445fc4
Stuart Leeks <stuartle@microsoft.com>
2024-02-05 15:48:36 +0000

    Add vcr

--- src/aoai-simulated-api/main.py
+++ src/aoai-simulated-api/main.py
@@ -1,9 +1,12 @@
 import os
 import requests
-from fastapi import FastAPI, Request
+import vcr
+from fastapi import FastAPI, Request, Response
 
 api_key = os.getenv("AZURE_OPENAI_KEY")
 api_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+
+simulator_mode = os.getenv("SIMULATOR_MODE") or "replay"
 
 if api_key is None:
     print("AZURE_OPENAI_KEY is not set", flush=True)
@@ -13,7 +16,103 @@ if api_endpoint is None:
     print("AZURE_OPENAI_ENDPOINT is not set", flush=True)
     exit(1)
 
+allowed_simulator_modes = ["replay", "record", "generate"]
+if simulator_mode not in allowed_simulator_modes:
+    print(f"SIMULATOR_MODE must be one of {allowed_simulator_modes}", flush=True)
+    exit(1)
+
+vcr_filtered_request_headers = [
+    # ignore api-key and don't persist!
+    "api-key",
+    # ignore these headers to reduce noise
+    "user-agent",
+    "x-stainless-arch",
+    "x-stainless-async",
+    "x-stainless-lang",
+    "x-stainless-os",
+    "x-stainless-package-version",
+    "x-stainless-runtime",
+    "x-stainless-runtime-version",
+    "connection",
+    "Content-Length",
+    "accept-encoding",
+]
+
+var_filtered_response_headers = [
+    # TODO - which (if any) of these do we care about? Any that we want to include in the responses?
+    "apim-request-id",
+    "azureml-model-session",
+    "x-accel-buffering",
+    "x-content-type-options",
+    "x-ms-client-request-id",
+    "x-ms-region",
+    "x-request-id",
+    "Cache-Control",
+    "Content-Length",
+]
+
+
+def before_record_response(response):
+    print("before_record_response", response, flush=True)
+    for header in var_filtered_response_headers:
+        if response["headers"].get(header):
+            del response["headers"][header]
+    return response
+    # response["headers"].clear()
+    # return response
+
+
+async def handle_with_vcr(request: Request, response: Response):
+    # https://vcrpy.readthedocs.io/en/latest/usage.html#record-modes
+    if simulator_mode == "record":
+        # add new requests but replay existing ones - can delete the file to start over
+        record_mode = "new_episodes"
+    elif simulator_mode == "replay":
+        # don't record new requests, only replay existing ones
+        record_mode = "none"
+    else:
+        raise NotImplementedError("simulator mode not implemented: " + simulator_mode)
+
+    my_vcr = vcr.VCR(
+        before_record_response=before_record_response,
+        filter_headers=vcr_filtered_request_headers,
+        record_mode=record_mode,
+        cassette_library_dir="cassettes",  # TODO: enable configuring this from an environment variable
+    )
+    # can use "test.json", serializer="json" for JSON cassettes
+    # TODO: determine the cassette name from the request
+    with my_vcr.use_cassette("test.yaml"):
+        # create new HTTP request to api_endpoint. copy headers, body, etc from request
+        url = api_endpoint
+        if url.endswith("/"):
+            url = url[:-1]
+        url += request.url.path + "?" + request.url.query
+
+        # Copy most headers, but override auth
+        fwd_headers = {
+            k: v
+            for k, v in request.headers.items()
+            if k not in ["host", "authorization"]
+        }
+        fwd_headers["api-key"] = api_key
+
+        body = await request.body()
+
+        fwd_response = requests.request(
+            request.method,
+            url,
+            headers=fwd_headers,
+            data=body,
+        )
+
+        # TODO - what customisation do we want to apply? Overriding tokens remaining etc?
+        for k, v in fwd_response.headers.items():
+            response.headers[k] = v
+        return fwd_response.json()
+
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -21,28 +120,11 @@ async def root():
 
 
 @app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def catchall(request: Request):
-    print("⚠️ unhandled route: " + request.url.path, flush=True)
-    body = await request.body()
+async def catchall(request: Request, response: Response):
+    print("⚠️ handling route: " + request.url.path, flush=True)
 
-    fwd_headers = { k: v for k, v in request.headers.items() if k not in ["host", "authorization"]}
-    fwd_headers["api-key"] = api_key
+    if simulator_mode == "generate":
+        raise NotImplementedError("generate mode not implemented")
 
-    # create new HTTP request to api_endpoint. copy headers, body, etc from request
-    url = api_endpoint
-    if url.endswith("/"):
-        url = url[:-1]
-    url += request.url.path + "?" + request.url.query
-    print("Forwarding to: " + url, flush=True)
-    print("Forwarding headers: " + str(fwd_headers), flush=True)
-
-    response = requests.request(
-        request.method,
-        url,
-        headers=fwd_headers,
-        data=body
-    )
-    print(f"Response ({response.status_code}):", flush=True)
-    print(response.headers, flush=True)
-    print(response.text, flush=True)
-    return response.text
+    if simulator_mode == "record" or simulator_mode == "replay":
+        return await handle_with_vcr(request, response)
--- src/test-client/app.py
+++ src/test-client/app.py
@@ -7,17 +7,17 @@ api_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 
 print("Connecting to: " + api_endpoint)
 client = AzureOpenAI(
-    api_key=api_key,  
-    api_version="2023-12-01-preview",
-    azure_endpoint = api_endpoint
-    )
-    
-deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT") #This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment. 
-    
-# Send a completion call to generate an answer
-print('Sending a test completion job')
-start_phrase = 'Tell me a joke'
-response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=10)
-print(response)
-# print(start_phrase+response.choices[0].text)
+    api_key=api_key, api_version="2023-12-01-preview", azure_endpoint=api_endpoint
+)
 
+# This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment.
+deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+
+# Send a completion call to generate an answer
+print("Sending a test completion job")
+start_phrase = "Tell me a joke"
+
+response = client.completions.create(
+    model=deployment_name, prompt=start_phrase, max_tokens=10
+)
+print(response)
