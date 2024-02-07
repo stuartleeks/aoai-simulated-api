2df9968fbb9b9491544eca61bdd26eac24034dce
Stuart Leeks <stuartle@microsoft.com>
2024-02-07 09:40:01 +0000

    Add chat completion to test client

--- src/test-client/app.py
+++ src/test-client/app.py
@@ -13,11 +13,28 @@ client = AzureOpenAI(
 # This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment.
 deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 
-# Send a completion call to generate an answer
-print("Sending a test completion job")
-start_phrase = "Tell me a joke"
+def send_test_completion():
+    # Send a completion call to generate an answer
+    print("Sending a test completion job")
+    response = client.completions.create(
+        model=deployment_name, prompt="A good DAD joke is ....", max_tokens=10
+    )
+    print(response)
 
-response = client.completions.create(
-    model=deployment_name, prompt=start_phrase, max_tokens=10
-)
-print(response)
+    print("Sending another test completion job")
+    response = client.completions.create(
+        model=deployment_name, prompt="A good story opening is!", max_tokens=10
+    )
+    print(response)
+
+
+def send_test_chat_completion():
+    response = client.chat.completions.create(
+        model=deployment_name, 
+        messages=[
+            {"role":"user", "content":"What is the meaning of life?"}
+        ])
+    print(response)
+
+
+send_test_chat_completion()
