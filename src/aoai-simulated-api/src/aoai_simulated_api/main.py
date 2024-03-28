9033d4d339f688f91ae14a474abc23bfa435e1b1
Stuart Leeks <stuartle@microsoft.com>
2024-03-28 18:22:18 +0000

    Fixup imports

--- src/aoai-simulated-api/src/aoai_simulated_api/main.py
+++ src/aoai-simulated-api/src/aoai_simulated_api/main.py
@@ -1,7 +1,8 @@
 import logging
 import os
 
-from aoai_simulated_api.app_builder import get_config_from_env_vars, get_simulator
+from aoai_simulated_api.config import get_config_from_env_vars
+from aoai_simulated_api.app_builder import get_simulator
 
 log_level = os.getenv("LOG_LEVEL") or "INFO"
 
